ad36aacd7f6660c9cba5aba33fa850b743a42ba4
julianboyne <julianboyne11@gmail.com>
2022-10-11 19:02:04 -0400

    add minor css for cards

--- src/components/StudentList/Student/Student.jsx
+++ src/components/StudentList/Student/Student.jsx
@@ -3,15 +3,17 @@ import Card from 'react-bootstrap/Card';
 
 const Student = ({student}) => {
   return (
-    <Card style={{ width: '18rem' }}>
-      <Card.Body>
-        <Card.Title>{student.name}</Card.Title>
-        <Card.Text>
+    <div>
+      <Card style={{ width: '18rem' }}>
+        <Card.Body>
+          <Card.Title>{student.name}</Card.Title>
+          <Card.Text>
           {student.bio}
-        </Card.Text>
-        {student.scores.map(score => <Score key={score.date} score={score}/>)}
-      </Card.Body>
-    </Card>
+          </Card.Text>
+          {student.scores.map(score => <Score key={score.date} score={score}/>)}
+        </Card.Body>
+      </Card>
+    </div>
   );
 }
 
